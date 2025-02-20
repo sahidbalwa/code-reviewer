449e976c22c2aabaa9ad2b8ad5061f8c2af09df1
sahidbalwa <138686668+sahidbalwa@users.noreply.github.com>
2025-02-20 11:12:03 +0530

    first commit

---
+++ BackEnd/src/routes/ai.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const aiController = require("../controllers/ai.controllers")
+
+const router = express.Router();
+
+
+router.post("/get-review", aiController.getReview)
+
+
+module.exports = router;    
